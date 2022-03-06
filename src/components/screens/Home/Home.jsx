9c458b9ef86aa5b04ed3f6620917d24800fdafa1
EveryBarry <burenkov.igor@gmail.com>
2022-03-06 18:14:26 +0300

    feat: add functional + state

--- src/components/screens/Home/Home.jsx
+++ src/components/screens/Home/Home.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ToDoItem from './item/ToDoItem';
 
-const todos = [
+const data = [
     {
         id: 1,
         title: 'some todo 1',
@@ -14,16 +14,35 @@ const todos = [
     },
     {
         id: 3,
-        title: 'some todo 3 ',
+        title: 'some todo 3',
         isCompleted: false,
     },
 ]
 
 const Home = () => {
+    const [todos, setTodos] = useState(data);
+
+    const changeTodo = (id) => {
+        const copy = [...todos];
+        const current = copy.find(t => t.id === id);
+        current.isCompleted = !current.isCompleted;
+        setTodos(copy);
+    }
+
+    const deleteTodo = (id) => {
+        setTodos([...todos].filter(t => t.id !== id))
+    }
+
+    // console.log(todos);
+
     return (
-        <div className="bg-gray-900 h-screen text-white">
-            {todos.map((todo) => (
-                <ToDoItem key={todo.id} todo={todo} />
+        <div className="text-white w-4/5 mx-auto">
+            <h1 className="text-2xl font-bold text-center mb-10">ToDo React Tailwind</h1>
+            {data.map((todo) => (
+                <ToDoItem key={todo.id} todo={todo}
+                    changeTodo={changeTodo}
+                    deleteTodo={deleteTodo}
+                />
             ))}
         </div>
     );
--- src/components/screens/Home/item/ToDoItem.jsx
+++ src/components/screens/Home/item/ToDoItem.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import Check from './Check';
+import { BsTrash } from 'react-icons/bs'
 
-const Todoitem = ({ todo }) => {
+const Todoitem = ({ todo, changeTodo }) => {
     return (
-        <div>
-            <Check />
-            {todo.title}
-        </div>
+        <button className='flex items-center justify-between mb-4 rounded-2xl bg-gray-800 p-5 w-full'
+            onClick={() => changeTodo(todo.id)}
+        >
+            <span className='flex items-center' >
+                <Check isCompleted={todo.isCompleted} />
+                <span className={`${todo.isCompleted ? 'line-through' : ''}`}>{todo.title}</span>
+            </span>
+            <BsTrash size={22} className='text-gray-900' />
+        </button>
     );
 }
 
