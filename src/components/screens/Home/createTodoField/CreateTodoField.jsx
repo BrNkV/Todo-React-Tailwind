5cc3aa883b4be9ab3fc15f834a593fe255bdfad5
EveryBarry <burenkov.igor@gmail.com>
2022-03-06 18:48:00 +0300

    feat: delete + create ToDo

---
+++ src/components/screens/Home/createTodoField/CreateTodoField.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreateTodoField = ({addTodo}) => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default CreateTodoField
--- src/components/screens/Home/item/ToDoItem.jsx
+++ src/components/screens/Home/item/ToDoItem.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import Check from './Check';
 import { BsTrash } from 'react-icons/bs'
 
-const Todoitem = ({ todo, changeTodo }) => {
+const Todoitem = ({ todo, changeTodo, deleteTodo }) => {
     return (
-        <button className='flex items-center justify-between mb-4 rounded-2xl bg-gray-800 p-5 w-full'
+        <div className='flex items-center justify-between mb-4 rounded-2xl bg-gray-800 p-5 w-full'
             onClick={() => changeTodo(todo.id)}
         >
-            <span className='flex items-center' >
+            <button className='flex items-center' >
                 <Check isCompleted={todo.isCompleted} />
                 <span className={`${todo.isCompleted ? 'line-through' : ''}`}>{todo.title}</span>
-            </span>
-            <BsTrash size={22} className='text-gray-900' />
-        </button>
+            </button>
+            <button
+                onClick={() => deleteTodo(todo.id)}
+            >
+                <BsTrash size={22} className='text-gray-600 hover:text-red-500 transition-colors ease-in-out duration-300' />
+            </button>
+        </div>
     );
 }
 
